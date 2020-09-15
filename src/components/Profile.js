6cb13139fef2f029ba2b38ca5f0add11c2b31c5e
Tessie Urtubia <gittessie@users.noreply.github.com>
2020-09-15 14:46:26 -0400

    fix profile edit page

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,32 +1,44 @@
 import * as React from "react";
-import { useUserContext } from "../contexts/user";
+import { useUserContext, useSetUserContext } from "../contexts/user";
 
 /**
  * This should update the user context with the new values for email and name
  */
 const Profile = () => {
   const user = useUserContext();
+  const setUserContext = useSetUserContext();
+  const [updatedUser, setUpdatedUser] = React.useState({
+    email: user.email,
+    name: user.name
+  });
+  const [showSuccess, setShowSuccess] = React.useState(false);
 
   return (
     <div>
       <h1>Edit your profile</h1>
+      {showSuccess && <p>Profile updated!</p>}
       <form
         onSubmit={e => {
           e.preventDefault();
+          setUserContext({
+            ...updatedUser
+          });
+          setShowSuccess(true);
         }}
       >
         <input
           name="email"
-          value={user.email}
+          value={updatedUser.email}
           onChange={e => {
-            user.email = e.target.value;
+            setUpdatedUser({...updatedUser, email: e.target.value})
           }}
+          type='email'
         />
         <input
           name="name"
-          value={user.name}
+          value={updatedUser.name}
           onChange={e => {
-            user.name = e.target.value;
+            setUpdatedUser({...updatedUser, name: e.target.value})
           }}
         />
         <button type="submit">Submit</button>
